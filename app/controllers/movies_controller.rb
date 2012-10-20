0fc75df455d67b43af8b3cd4b71f0f85153e4ed1
puneethrai <npuneethrai@yahoo.co.in>
2012-10-20 09:11:35 -0700

    Modifying

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,36 +7,52 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+        # redirect if ratings / table sort not specified but saved in session
     @ratings = params[:ratings]
-    @all_ratings = ['G','PG','PG-13','R']
-    puts @ratings
-    if(@ratings==nil)
-      @ratings = {"G"=>"1","PG"=>"1","PG-13"=>"1","R"=>"1"}
+    @sort_by = params[:by]
+    session_ratings = (@ratings.nil? and !session[:ratings].nil?)
+    session_sort_by = (@sort_by.nil? and !session[:sort_by].nil?)
+    if session_ratings or session_sort_by
+      if session_ratings
+        # no ratings filter selected by user
+        # session exists with ratings filter
+        @ratings = session[:ratings]
+      end
+      if session_sort_by
+        # no table sort specified by user
+        # session exists with table sort
+        @sort_by = session[:sort_by]
+      end
+      redirect_to :action => 'index', :ratings => @ratings, :by => @sort_by
     end
-    if(params[:commit]=="Refresh")
-      if(params[:ratings])
-       ratings=params[:ratings].keys
-       @movies = Movie.where("rating IN (?)", ratings)
-         if(params[:by])
-            @movies = @movies.order(params[:by])
-            @sort=params[:by]
-         end
-       else
-         @movies = []
-       end
-    elsif(params[:by]=="title")
-	     @movies = Movie.order(params[:by]).all
-    	 @sort="title"
-    elsif(params[:by]=="rating")
-	     @movies = Movie.order(params[:by]).all
-	     @sort = "rating"
-    elsif(params[:by]=="release_date")
-	     @movies = Movie.order(params[:by]).all
-	     @sort = "release_date"
     
-    end
+    # query database for all movies matching criteria
+    @all_ratings = ['G','PG','PG-13','R']
+    find_params = Hash.new
+    
+    if @ratings
+      # user requested a rating filter
+      ratings = @ratings.keys
+      if ratings.length > 0
+        find_params[:conditions] = ["rating IN (?)", ratings]
+      end
       
+      # save the ratings filter in a session
+      session[:ratings] = @ratings
+    else
+      @ratings = Hash.new
+    end
+    
+    if (@sort_by == 'title') or (@sort_by == 'release_date')
+      # user requested a table sort
+      find_params[:order] = @sort_by
+      
+      # save tje table sort in a session
+      session[:sort_by] = @sort_by
+    end
+    
+    @movies = Movie.find(:all, find_params)
+  
 
   end
 
