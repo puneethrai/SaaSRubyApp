8bb20d1478733e08d02d5ae7bc115d0ede291eff
puneethrai <npuneethrai@yahoo.co.in>
2012-10-22 11:38:10 -0700

    Modifying

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  
+  @MoviePage=1
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
